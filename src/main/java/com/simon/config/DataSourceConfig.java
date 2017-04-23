89355d2ca4f5df53eaf3ea50aea6a899ddb3e242
SimonSun <simon.sun.dev@hotmail.com>
2017-04-24 01:08:37 +0800

    切换数据源MySQL到PostgreSQL。

--- src/main/java/com/simon/config/DataSourceConfig.java
+++ src/main/java/com/simon/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.simon.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +21,21 @@ public class DataSourceConfig {
     private Environment env;
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    public DruidDataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        dataSource.setMaxActive(env.getProperty("spring.datasource.druid.max-active", Integer.class));
+        dataSource.setInitialSize(env.getProperty("spring.datasource.druid.initial-size", Integer.class));
+        dataSource.setMaxWait(env.getProperty("spring.datasource.druid.max-wait", Integer.class));
+        dataSource.setMinIdle(env.getProperty("spring.datasource.druid.min-idle", Integer.class));
+        dataSource.setRemoveAbandoned(env.getProperty("spring.datasource.druid.remove-abandoned", Boolean.class));
+        dataSource.setRemoveAbandonedTimeout(env.getProperty("spring.datasource.druid.remove-abandoned-timeout", Integer.class));
+        dataSource.setConnectionProperties(env.getProperty("spring.datasource.druid.connection-properties"));
+        dataSource.setTestWhileIdle(env.getProperty("spring.datasource.druid.test-while-idle", Boolean.class));
         return dataSource;
     }
 
