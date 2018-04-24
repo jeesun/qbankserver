752b83e71346999468d6810c8ab83704f1b383e7
SimonSun <simon.sun.dev@hotmail.com>
2018-04-24 17:13:15 +0800

    删除定义的无用字符串变量；把阿里大于的配置信息放到properties中。

--- src/main/java/com/simon/controller/VeriCodeController.java
+++ src/main/java/com/simon/controller/VeriCodeController.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +31,27 @@ public class VeriCodeController {
 
     @Autowired
     private VeriCodeRepository veriCodeRepository;
+
+    @Value("${com.alibaba.dayu.url.sandbox}")
+    private String DAYU_URL_SANDBOX;
+
+    @Value("${com.alibaba.dayu.url.real}")
+    private String DAYU_URL_REAL;
+
+    @Value("${com.alibaba.dayu.app-key}")
+    private String DAYU_APP_KEY;
+
+    @Value("${com.alibaba.dayu.app-secret}")
+    private String DAYU_APP_SECRET;
+
+    @Value("${com.alibaba.dayu.sms-type}")
+    private String DAYU_SMS_TYPE;
+
+    @Value("${com.alibaba.dayu.sms-free-sign-name}")
+    private String DAYU_SMS_FREE_SIGN_NAME;
+
+    @Value("${com.alibaba.dayu.sms-template-code}")
+    private String DAYU_SMS_TEMPLATE_CODE;
 
     @ApiOperation(value = "获取验证码", notes = "验证码有效时间是30分钟，验证码在失效前5分钟就会重新生成一个返回，给用户通过验证码修改密码足够的时间")
     /*@ApiImplicitParam(name = "phone", value = "用户手机号", required = true, dataType = "string")*/
@@ -53,14 +75,14 @@ public class VeriCodeController {
             }
         }
         TaobaoClient client = new DefaultTaobaoClient(
-                ServerContext.DAYU_URL_REAL, ServerContext.DAYU_APP_KEY, ServerContext.DAYU_APP_SECRET);
+                DAYU_URL_REAL, DAYU_APP_KEY, DAYU_APP_SECRET);
         AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
         req.setExtend("");
-        req.setSmsType("normal");
-        req.setSmsFreeSignName(ServerContext.DAYU_SMS_FREE_SIGN_NAME);
+        req.setSmsType(DAYU_SMS_TYPE);
+        req.setSmsFreeSignName(DAYU_SMS_FREE_SIGN_NAME);
         req.setSmsParamString("{veriCode:'"+veriCode.getCode()+"'}");
         req.setRecNum(phone);
-        req.setSmsTemplateCode(ServerContext.DAYU_SMS_TEMPLATE_CODE);
+        req.setSmsTemplateCode(DAYU_SMS_TEMPLATE_CODE);
         AlibabaAliqinFcSmsNumSendResponse rsp = null;
         try {
             rsp = client.execute(req);
--- src/main/java/com/simon/utils/ServerContext.java
+++ src/main/java/com/simon/utils/ServerContext.java
@@ -12,24 +12,5 @@ public class ServerContext {
     public static final String MORE_INFO = "moreInfo";
     public static final String IP="120.25.152.172";
 
-    public static final Integer SIGN_UP_STATUS = 1;
-    public static final Integer SIGN_IN_STATUS = 2;
-    public static final Integer SIGN_OUT_STATUS = 3;
-
-    public static final String DAYU_URL_SANDBOX = "http://gw.api.tbsandbox.com/router/rest";
-    public static final String DAYU_URL_REAL = "http://gw.api.taobao.com/router/rest";
-    public static final String DAYU_APP_KEY = "23656595";
-    public static final String DAYU_APP_SECRET = "f5b33f982cc1e5a8becbf37cd03e51fb";
-    public static final String DAYU_SMS_FREE_SIGN_NAME = "益题库";
-    public static final String DAYU_SMS_TEMPLATE_CODE = "SMS_50225027";
-
-    public static final String JIGUANG_APP_KEY = "2cb1b6f5ee6c596abe813e49";
-    public static final String JIGUANG_MASTER_SECRET = "aa81152ecac5776f2ff6db91";
-
-    public static final Integer AUDIT_RESULT_WAIT = 0;
-    public static final Integer AUDIT_RESULT_SUCCESS = 1;
-    public static final Integer AUDIT_RESULT_REFUSED = 2;
-    public static final Integer AUDIT_RESULT_RESUBMIT = 3;
-
     public static final String OAUTH_URI = "http://localhost:8182/oauth/token";
 }
