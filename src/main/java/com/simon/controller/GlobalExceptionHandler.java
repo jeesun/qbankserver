c497266059be7cbc57774beaeae6ea522b84fa29
SimonSun <simon.sun.dev@hotmail.com>
2018-05-31 11:01:46 +0800

    更新全局异常处理类。

--- src/main/java/com/simon/controller/GlobalExceptionHandler.java
+++ src/main/java/com/simon/controller/GlobalExceptionHandler.java
@@ -4,7 +4,10 @@ import com.simon.domain.ResultMsg;
 import org.apache.log4j.Logger;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
@@ -17,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
  * @create 2018-04-25 23:25
  **/
 
-@Order(Ordered.LOWEST_PRECEDENCE)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ExceptionHandlerExceptionResolver {
     private static Logger logger = Logger.getLogger(GlobalExceptionHandler.class);
@@ -26,6 +29,17 @@ public class GlobalExceptionHandler extends ExceptionHandlerExceptionResolver {
     public ResultMsg grantError(HttpServletRequest request, Exception e){
         ResultMsg resultMsg = new ResultMsg();
         resultMsg.setCode(500);
+        resultMsg.setMsg(e.getMessage());
+        resultMsg.setData(e.toString());
+        logger.error(e);
+        return resultMsg;
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResultMsg accessDeniedError(HttpServletRequest request, AccessDeniedException e){
+        ResultMsg resultMsg = new ResultMsg();
+        resultMsg.setCode(HttpStatus.FORBIDDEN.value());
         resultMsg.setMsg(e.getMessage());
         resultMsg.setData(e.toString());
         logger.error(e);
